fc883900376863a6b077bd9d1a44ef78358f7152
Tim Empey <timothyempey@gmail.com>
2023-01-04 10:39:20 -0800

    refactored intro card styling

--- src/index.js
+++ src/index.js
@@ -38,3 +38,16 @@ a.forEach(item => {
     cursor.classList.remove('hover');
     });
 })
+
+//  Nav Bar scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0";
+    } else {
+    document.querySelector("nav").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
